b07b20bb43de698d4240afa2bba782ac1d53744e
Enrique Flores <kikeoffline@gmail.com>
2021-03-19 17:51:41 -0600

    Added Three JS & Three Globe.

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,8 @@ import '../styles/styles.css'
 import log from './log.js'
 import globe from './globe'
 
+fetch('http://api.open-notify.org/iss-now.json?')
+  .then(response => response.json())
+  .then(data => console.log(data));
+
 log('EOF')
--- src/js/globe.js
+++ src/js/globe.js
@@ -1,108 +1,69 @@
-import * as THREE from 'three'
+import * as THREE from 'three';
+import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls.js';
+import ThreeGlobe from 'three-globe';
 
 
-  const container = document.getElementById("globe");
-  const canvas = container.getElementsByTagName("canvas")[0];
+    // Gen random data
+    const gData = [{
+      lat: '-51.2532',
+      lng: '-154.2825',
+      size: 0.1,
+      color: 'red'
+    }];
 
-  const globeRadius = 200;
-  const globeWidth = 4098 / 2;
-  const globeHeight = 1968 / 2;
+    const Globe = new ThreeGlobe({ animateIn: true})
+      .globeImageUrl('//unpkg.com/three-globe/example/img/earth-night.jpg')
+      .bumpImageUrl('//unpkg.com/three-globe/example/img/earth-topology.png')
+      .showAtmosphere(true)
+      .showGraticules(true)
+      .pointsData(gData)
+      .pointAltitude('size')
+      .pointColor('color');
 
-  function convertFlatCoordsToSphereCoords(x, y) {
-    let latitude = ((x - globeWidth) / globeWidth) * -180;
-    let longitude = ((y - globeHeight) / globeHeight) * -90;
-    latitude = (latitude * Math.PI) / 180;
-    longitude = (longitude * Math.PI) / 180;
-    const radius = Math.cos(longitude) * globeRadius;
+    const issLatLng = Globe.getCoords(gData[0].lat, gData[0].lng);
 
-    return {
-      x: Math.cos(latitude) * radius,
-      y: Math.sin(longitude) * globeRadius,
-      z: Math.sin(latitude) * radius
-    };
-  }
+    console.log(issLatLng);
 
-  function makeMagic(points) {
-    const { width, height } = container.getBoundingClientRect();
+    // Setup renderer
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.getElementById('globe').appendChild(renderer.domElement);
 
-    // 1. Setup scene
+    // Setup scene
     const scene = new THREE.Scene();
-    // 2. Setup camera
-    const camera = new THREE.PerspectiveCamera(45, width / height);
-    // 3. Setup renderer
-    const renderer = new THREE.WebGLRenderer({
-      canvas,
-      antialias: true
-    });
-    renderer.setSize(width, height);
-    // 4. Add points to canvas
-    // - Single geometry to contain all points.
-    const mergedGeometry = new THREE.Geometry();
-    // - Material that the dots will be made of.
-    const pointGeometry = new THREE.SphereGeometry(0.5, 1, 1);
-    const pointMaterial = new THREE.MeshBasicMaterial({
-      color: "#888",
-    });
+    scene.add(Globe);
+    scene.add(new THREE.AmbientLight(0xbbbbbb));
+    scene.add(new THREE.DirectionalLight(0xffffff, 0.2));
 
-    for (let point of points) {
-      const { x, y, z } = convertFlatCoordsToSphereCoords(
-        point.x,
-        point.y,
-        width,
-        height
-      );
+    // Setup camera
+    const camera = new THREE.PerspectiveCamera();
+    camera.aspect = window.innerWidth/window.innerHeight;
+    camera.updateProjectionMatrix();
+    camera.position.z = 300;
 
-      if (x && y && z) {
-        pointGeometry.translate(x, y, z);
-        mergedGeometry.merge(pointGeometry);
-        pointGeometry.translate(-x, -y, -z);
-      }
-    }
+    // Add camera controls
+    const tbControls = new TrackballControls(camera, renderer.domElement);
+    tbControls.minDistance = 101;
+    tbControls.rotateSpeed = 5;
+    tbControls.zoomSpeed = 0.8;
 
-    const globeShape = new THREE.Mesh(mergedGeometry, pointMaterial);
-    scene.add(globeShape);
 
-    container.classList.add("globe");
+    // const issX  = gData[0].lat / 3.14;
+    const issX  = issLatLng.x * Math.PI;
+    const issY  = issLatLng.y;
+    const issZ  = issLatLng.z;
+    
+    Globe.rotation.x = -0.5;
+    Globe.rotation.y = issX;
+    // Globe.rotation.z = issZ / 100;
 
-    // Setup orbital controls
-    camera.orbitControls = new THREE.OrbitControls(camera, canvas);
-    camera.orbitControls.enableKeys = false;
-    camera.orbitControls.enablePan = false;
-    camera.orbitControls.enableZoom = false;
-    camera.orbitControls.enableDamping = false;
-    camera.orbitControls.enableRotate = true;
-    camera.orbitControls.autoRotate = true;
-    camera.position.z = -265;
+    console.log(issX, issY, issZ);
 
-    function animate() {
-      // orbitControls.autoRotate is enabled so orbitControls.update
-      // must be called inside animation loop.
-      camera.orbitControls.update();
-      requestAnimationFrame(animate);
+    // Kick-off renderer
+    (function animate() {
+      // Frame cycle
+      tbControls.update();
       renderer.render(scene, camera);
-    }
-    animate();
-  }
-
-  function hasWebGL() {
-    const gl =
-      canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
-    if (gl && gl instanceof WebGLRenderingContext) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  function init() {
-    if (hasWebGL()) {
-      window
-        .fetch("/js/points.json")
-        .then(response => response.json())
-        .then(data => {
-          makeMagic(data.points);
-        });
-    }
-  }
-  init();
-
+      requestAnimationFrame(animate);
+      Globe.rotation.y -= 0.000005;
+    })();
