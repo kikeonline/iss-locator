92b19a544be90816c8aa3faf071e4678cbbf3b04
Enrique Flores <kikeoffline@gmail.com>
2021-04-10 16:49:59 -0600

    💚 Fix CI build form Netlify

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,32 @@
-const path = require("path");
-const webpack = require("webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  mode: "development",
+  mode: 'development',
   entry: {
-    app: "./src/js/app.js",
+    app: './src/js/app.js'
   },
   watch: true,
   watchOptions: {
     aggregateTimeout: 500,
     poll: 1000,
-    ignored: /node_modules/,
+    ignored: /node_modules/
   },
-  devtool: "source-maps",
+  devtool: 'source-maps',
   devServer: {
-    contentBase: path.join(__dirname, "src"),
+    contentBase: path.join(__dirname, 'src'),
     watchContentBase: true,
     hot: true,
     open: true,
-    inline: true,
+    inline: true
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Webpack Boilerplate",
-      template: path.resolve("./src/index.html"),
+      title: 'Webpack Boilerplate',
+      template: path.resolve('./src/index.html')
     }),
-    new webpack.HotModuleReplacementPlugin(),
+    new webpack.HotModuleReplacementPlugin()
   ],
   module: {
     rules: [
@@ -35,45 +35,45 @@ module.exports = {
         exclude: /node_modules/,
         use: [
           {
-            loader: "style-loader",
+            loader: 'style-loader'
           },
           {
-            loader: "css-loader",
+            loader: 'css-loader',
             options: {
-              importLoaders: 1,
-            },
+              importLoaders: 1
+            }
           },
           {
-            loader: "postcss-loader",
-          },
-        ],
+            loader: 'postcss-loader'
+          }
+        ]
       },
       {
         test: /\.m?js$/,
         exclude: /(node_modules|bower_components)/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-env"],
-          },
-        },
+            presets: ['@babel/preset-env']
+          }
+        }
       },
       {
         test: /\.(jpg|jpeg|gif|png|svg)$/,
         use: {
-          loader: "file-loader",
+          loader: 'file-loader',
           options: {
-            outputPath: "./images",
-            name: "[name].[ext]",
-          },
-        },
+            outputPath: './images',
+            name: '[name].[ext]'
+          }
+        }
       },
       {
         test: /\.html$/,
         use: {
-          loader: "html-loader",
-        },
-      },
-    ],
-  },
-};
+          loader: 'html-loader'
+        }
+      }
+    ]
+  }
+}
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,89 +1,89 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
-  mode: "production",
+  mode: 'production',
   devtool: false,
   entry: {
-    app: "./src/js/app.js",
+    app: './src/js/app.js'
   },
   output: {
-    filename: "[name].[contenthash].js",
-    path: path.resolve(__dirname, "dist"),
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist')
   },
   optimization: {
-    moduleIds: "hashed",
-    runtimeChunk: "single",
+    moduleIds: 'hashed',
+    runtimeChunk: 'single',
     splitChunks: {
       cacheGroups: {
         vendor: {
           test: /[\\/]node_modules[\\/]/,
-          name: "vendors",
-          chunks: "all",
-        },
-      },
-    },
+          name: 'vendors',
+          chunks: 'all'
+        }
+      }
+    }
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: "[name].bundle.css",
-      chunkFilename: "[id].css",
+      filename: '[name].bundle.css',
+      chunkFilename: '[id].css'
     }),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: "Webpack Boilerplate",
-      template: path.resolve("./src/index.html"),
-      favicon: "./src/images/favicon.ico",
-    }),
+      title: 'Webpack Boilerplate',
+      template: path.resolve('./src/index.html'),
+      favicon: './src/images/favicon.png'
+    })
   ],
   module: {
     rules: [
       {
         test: /\.css$/,
-        include: path.resolve(__dirname, "src"),
+        include: path.resolve(__dirname, 'src'),
         exclude: /node_modules/,
         use: [
-          "style-loader",
+          'style-loader',
           {
-            loader: MiniCssExtractPlugin.loader,
+            loader: MiniCssExtractPlugin.loader
           },
           {
-            loader: "css-loader",
+            loader: 'css-loader',
             options: {
-              importLoaders: 1,
-            },
+              importLoaders: 1
+            }
           },
-          "postcss-loader",
-        ],
+          'postcss-loader'
+        ]
       },
       {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-env"],
-          },
-        },
+            presets: ['@babel/preset-env']
+          }
+        }
       },
       {
         test: /\.(jpg|jpeg|gif|png|svg)$/,
         use: {
-          loader: "file-loader",
+          loader: 'file-loader',
           options: {
-            outputPath: "./images",
-            name: "[name].[ext]",
-          },
-        },
+            outputPath: './images',
+            name: '[name].[ext]'
+          }
+        }
       },
       {
         test: /\.html$/,
         use: {
-          loader: "html-loader",
-        },
-      },
-    ],
-  },
-};
+          loader: 'html-loader'
+        }
+      }
+    ]
+  }
+}
