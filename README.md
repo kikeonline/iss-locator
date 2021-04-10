7f728abbe3ad4dcce0dae4c6a1863817acd3f5e3
Enrique Flores <kikeoffline@gmail.com>
2021-04-10 17:17:30 -0600

    👷 CI build to Netlify

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # ISS Locator
 <a href="https://standardjs.com"><img src="https://img.shields.io/badge/code_style-standard-brightgreen.svg" alt="Standard - JavaScript Style Guide"></a>
+[![Netlify Status](https://api.netlify.com/api/v1/badges/ace38cb4-f72f-4324-84d7-aa06446d46be/deploy-status)](https://app.netlify.com/sites/iss-3dglobe-location/deploys)
 
 <h1 align="center">
   <img src="https://raw.githubusercontent.com/kikeonline/iss-locator/main/screenshot.gif" alt="ISS Locator" width="800">
@@ -7,6 +8,8 @@
 </h1>
 
 International Space Station geolocation in 3D Globe, WIP.
+
+Live build: iss-3dglobe-location.netlify.app
 
 - http://open-notify.org/Open-Notify-API/
 - http://api.geonames.org/
--- src/js/fetchCountry.js
+++ src/js/fetchCountry.js
@@ -4,7 +4,7 @@ const element = document.getElementById('country-label')
 
 export default class GetCountry {
   getCountry (lat, lng) {
-    window.fetch(`http://api.geonames.org/countryCodeJSON?lat=${lat}&lng=${lng}&username=${GEONAMES_USERNAME}`)
+    window.fetch(`https://secure.geonames.org/countryCodeJSON?lat=${lat}&lng=${lng}&username=${GEONAMES_USERNAME}`)
       .then(response => response.json())
       .then(conuntryResult => {
         // console.log(conuntryResult)
@@ -12,7 +12,7 @@ export default class GetCountry {
           console.log('Country: ' + conuntryResult.countryName)
           element.innerHTML = conuntryResult.countryName
         } else {
-          const getOcean = window.fetch(`http://api.geonames.org/oceanJSON?lat=${lat}&lng=${lng}&username=${GEONAMES_USERNAME}`)
+          const getOcean = window.fetch(`https://secure.geonames.org/oceanJSON?lat=${lat}&lng=${lng}&username=${GEONAMES_USERNAME}`)
           getOcean.then(response => response.json())
             .then(oceanResult => {
               console.log('Ocean: ' + oceanResult.ocean.name)
